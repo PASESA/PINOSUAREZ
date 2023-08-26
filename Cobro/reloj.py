289db40f500ce77677b6eba90ae199f68f682857
Noé Zúñiga <noe.zm2000@gmail.com>
2023-08-26 16:48:29 -0600

    Docs: se añaden cmentarios a la clase dekl reloj

--- Cobro/reloj.py
+++ Cobro/reloj.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from math import radians, cos, sin
 
 class BlinkingLabel:
+    # Clase para manejar el parpadeo de etiquetas
 
     def __init__(self):
         self.label = None
@@ -57,6 +58,8 @@ class BlinkingLabel:
             print("se de tiene parpadeo")
 
 class RelojAnalogico:
+    # Clase para la interfaz de reloj analógico
+
     def __init__(self):
         """Inicializa la interfaz del reloj analógico."""
         self.root = tk.Toplevel()
@@ -67,7 +70,7 @@ class RelojAnalogico:
         self.blink_interval = 500
 
         self.blinking_label = BlinkingLabel()
-        self.root.geometry("+1650+0") 
+        # self.root.geometry("+1650+0")
 
         self.interface()
 
@@ -143,7 +146,8 @@ class RelojAnalogico:
 
         # Dibujar las divisiones de los minutos en el canvas de fondo
         for i in range(60):
-            angle = radians(90 - i * 6)  # Corrección para el 0 en la parte superior
+            # Corrección para el 0 en la parte superior
+            angle = radians(90 - i * 6)
             x1 = 150 + 100 * cos(angle)
             y1 = 150 - 100 * sin(angle)
             x2 = 150 + 120 * cos(angle)
@@ -161,7 +165,8 @@ class RelojAnalogico:
 
         # Dibujar las divisiones en 4 partes iguales en el canvas de fondo
         for i in range(4):
-            angle_division = radians(i * 90)  # Ángulo para cada división (0, 90, 180, 270 grados)
+            # Ángulo para cada división (0, 90, 180, 270 grados)
+            angle_division = radians(i * 90)
             x1_division = 150 + 100 * cos(angle_division)
             y1_division = 150 - 100 * sin(angle_division)
             x2_division = 150 + 120 * cos(angle_division)  # Ajustar la longitud de las divisiones
@@ -211,40 +216,49 @@ class RelojAnalogico:
         self.update_background(0, False)
 
     def update_background(self, all_minutes: int, more_than_hour: bool) -> None:
-        """Actualiza el fondo del reloj con el color correspondiente según los minutos transcurridos.
+        """
+        Actualiza el fondo del reloj con el color correspondiente según los minutos transcurridos.
 
         Args:
-            all_minutes (int): Minutos transcurridos.
+            all_minutes (int): Minutos transcurridos en su totalidad.
+            more_than_hour (bool): True si ha pasado más de una hora, False si no.
 
         Returns:
             None
         """
-        # Dibujar el área anterior a la manecilla de minutos con el color correspondiente
+        # Eliminar el área anterior a la manecilla de minutos con el color correspondiente
         self.canvas_background.delete("previous_area")
+
+        # Calcular el ángulo de inicio y la longitud del arco
         start_angle = 90 - all_minutes * 6
         extent = all_minutes * 6
         color = None
+        label_select = None
 
-        if not more_than_hour:
+        if not more_than_hour or all_minutes < 60:
+            # Establecer el color y la etiqueta para la primera hora
             color = self.color_first_hour
             label_select = self.color_box_0
-
         else:
+            # Calcular minutos y asignar color y etiqueta según el rango
             _, minutes = divmod(all_minutes, 60)
-            if minutes < 16 and minutes >= 1:
+            if 1 <= minutes < 16:
                 color = self.colors[0]
                 label_select = self.color_box_1
-            elif minutes < 31 and minutes >= 16:
+            elif 16 <= minutes < 31:
                 color = self.colors[1]
                 label_select = self.color_box_2
-            elif minutes < 46 and minutes >= 31:
+            elif 31 <= minutes < 46:
                 color = self.colors[2]
                 label_select = self.color_box_3
             else:
                 color = self.colors[3]
                 label_select = self.color_box_4
 
+        # Crear un arco en el canvas con el color y el ángulo calculados
         self.canvas_background.create_arc(50, 50, 250, 250, start=start_angle, extent=extent, fill=color, outline=color, tags="previous_area")
+
+        # Actualizar la posición de la manecilla de minutos en el reloj
         self.update_clock(all_minutes)
 
         return label_select
@@ -273,11 +287,11 @@ class RelojAnalogico:
         self.canvas_background.coords(self.minute_hand, 150, 150, x, y)
 
     def set_time(self, entrada: str = "00:00", salida: str = "00:00", hour: int = 0, minute: int = 0, importe: int = 0) -> None:
-        """Simula el paso del tiempo y actualiza la interfaz del reloj.
+        """Simula el paso del tiempo y actualiza la interfaz del reloj con los nuevos valores.
 
         Args:
-            entrada (str): Hora de entrada.
-            salida (str): Hora de salida.
+            entrada (str): Hora de entrada en formato HH:MM.
+            salida (str): Hora de salida en formato HH:MM.
             hour (int): Horas transcurridas.
             minute (int): Minutos transcurridos.
             importe (int): Importe total.
@@ -285,46 +299,56 @@ class RelojAnalogico:
         Returns:
             None
         """
+        # Se reestablece valor de tiqueta de color seleccionada para el parpadeo
         label_select = None
+
+        # Restablecer el fondo del reloj y configurar la hora inicial
         self.update_background(0, False)
 
         self.hour = hour
         more_than_hour = False
+
+        # Ajustar la hora si ha pasado más de una hora y no es la primera hora completa
         if hour >= 1:
             hour = 1
             more_than_hour = True
             if self.hour == 1 and minute == 0:
                 more_than_hour = False
 
+        # Calcular el tiempo total en minutos y configurar el tiempo actual
         total_minutes = hour * 60 + minute
         current_minutes = 0
 
+        # Ajustar el tiempo total si es la primera hora incompleta
         if self.hour == 0 and minute < 60:
             total_minutes = 60
 
-        time_per_frame = 0.5 / (total_minutes)
+        # Calcular el tiempo por cuadro de animación
+        time_per_frame = 0.3 / (total_minutes)
 
+        # Iterar para simular el paso del tiempo
         while current_minutes <= total_minutes:
             label_select = self.update_background(current_minutes, more_than_hour)
             self.root.update()  # Actualizar la ventana
             current_minutes += 1
             self.root.after(int(time_per_frame * 1000))  # Convertir a milisegundos
 
+            # Actualizar las horas cada 60 minutos
             if current_minutes % 60 == 0:
                 self.label_horas.config(text = f"{self.hour} Hrs", font=("Arial", 20))
 
-
+        # Iniciar el parpadeo de la etiqueta de color seleccionada
         self.blinking_label.start_blinking(label_select, self.blink_interval)
 
-        # Actualizar el Label de tiempo con el tiempo final
+        # Crear una representación de tiempo en formato "HH Hrs MM Min"
         time_str = "{:02d} Hrs {:02d} Min".format(self.hour, minute)
 
+        # Actualizar etiquetas con los valores de tiempo y importe
         self.label_tiempo_entrada.config(text = f"Entrada: {entrada[:-3]} Hrs")
         self.label_tiempo_salida.config(text = f"Salida: {salida[:-3]} Hrs")
 
         self.label_tiempo_total.config(text = f"{time_str}")
         self.label_importe_total.config(text = f"${importe}.00")
-
 
     def update_data(self, tarifa: str, importe: int) -> None:
         """Actualiza la información de la tarifa y el importe en la interfaz.
@@ -361,16 +385,16 @@ class RelojAnalogico:
 
 
 
-# Ejemplo de uso:
-reloj = RelojAnalogico()
+# # Ejemplo de uso:
+# reloj = RelojAnalogico()
 
-entrada = "01:00:00"
-salida = "02:30:00"
-importe = "100"
-hora = 1
-minuto = 1
+# entrada = "01:00:00"
+# salida = "02:30:00"
+# importe = "100"
+# hora = 2
+# minuto = 31
 
-reloj.set_time(entrada=entrada, salida=salida, hour= hora,minute= minuto, importe=importe)
-reloj.open_window()
+# reloj.set_time(entrada=entrada, salida=salida, hour= hora,minute= minuto, importe=importe)
+# reloj.open_window()
 
 
